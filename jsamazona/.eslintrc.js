931ecdbf42236f7b631327ae8afaec8ba57ad012
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-01-23 19:08:51 +0200

    adding eslint that doesnt work :)

---
+++ jsamazona/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2020:true
+    },
+    extends: ['airbnb-base'],
+    parserOpsions: {
+        sourceType: 'module',
+        ecmaVersion:11
+    }
+}
--- jsamazona/backend/server.js
+++ jsamazona/backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import data from './data'
+
 const app = express();
 app.use(cors());
 
